29b90b2de74093a353f03603f9200cdd9d5d4e13
bobycat22 <anthonyznj@gmail.com>
2022-04-03 08:47:48 -0400

    change file structure

---
+++ flutter_visionary/build/flutter_tts/generated/source/buildConfig/debug/com/tundralabs/fluttertts/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tundralabs.fluttertts;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.tundralabs.fluttertts";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ flutter_visionary/lib/generated_plugin_registrant.dart
@@ -0,0 +1,20 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:camera_web/camera_web.dart';
+import 'package:flutter_tts/flutter_tts_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  CameraPlugin.registerWith(registrar);
+  FlutterTtsPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
